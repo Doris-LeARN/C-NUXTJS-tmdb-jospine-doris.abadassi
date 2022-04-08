3e9bc56ef9e336d6779d183d812dea1d6182917f
Doris-LeARN <jospine-doris.abadassi@epitech.eu>
2022-04-08 20:11:09 +0100

    First commit of the first part of work

--- tmdb/components/MoviesPopular/MoviesPopularItem.vue
+++ tmdb/components/MoviesPopular/MoviesPopularItem.vue
@@ -2,42 +2,73 @@
     <div class="max-w-sm relative rounded overflow-hidden shadow-lg">
         <img class="md:w-full" :src="'https://image.tmdb.org/t/p/w500'+listMoviePopular.poster_path" :alt="listMoviePopular.title">
         <div class="px-6 py-4">
-            <div class="font-bold text-base mb-2">{{ listMoviePopular.title }}</div>
+            <div class="font-bold text-sm mb-2">{{ listMoviePopular.title }}</div>
             <p class="text-gray-700 text-base">
             
             </p>
         </div>
-        <div x-data="valeurProgress" class="absolute top-0 mt-56 left-0 ml-4 rounded-full inline-flex items-center justify-center overflow-hidden bg-black">
-            <!-- <div x-data="scrollProgress" class="absolute inline-flex items-center justify-center overflow-hidden rounded-full"> -->
-                <!-- Building a Progress Ring: https://css-tricks.com/building-progress-ring-quickly/ -->
-                <svg class="w-10 h-10">
-                    <circle class="text-gray-300" stroke-width="4" stroke="currentColor" fill="transparent" r="20" cx="20" cy="20"/>
-                    <circle class="text-green-500" stroke-width="4" :stroke-dasharray="circumference" :stroke-dashoffset="circumference - percent / 100 * circumference" stroke-linecap="round" stroke="currentColor" fill="transparent" r="20" cx="20" cy="20" />
-                </svg>
-                <span class="absolute text-xl text-blue-700" x-text="`${listMoviePopular.vote_average * 100}%`"></span>
-            <!-- </div> -->
+        <div class="absolute top-0 mt-56 left-0 ml-4 rounded-full inline-flex items-center justify-center overflow-hidden bg-black">
+            <ProgressRing :radius="20" :progress="progress" :stroke="2" />
+            <span class="absolute text-sm text-white">{{ Math.round(this.$props.listMoviePopular.vote_average * 100)/10 }}</span>
+            <span id="percent" class="absolute text-sm text-white">%</span>
         </div>
-        <div class="absolute top-0 mt-3 right-0 mr-2 border h-6 w-6 rounded-full bg-white">
+        <button class="absolute top-0 mt-3 right-0 mr-2 h-5 w-5 opacity-50 rounded-full bg-white hover:bg-blue-400 hover:opacity-100" type="button" v-on:click="toggleDropdownBtn()" ref="btnDropdownBtnRef">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
             </svg>
+        </button>
+        <div v-bind:class="{ 'hidden': !dropdownPopoverShow, 'block': dropdownPopoverShow }" id="dropdownDivider" class="z-10 w-44 bg-white rounded divide-y divide-gray-600 shadow dark:bg-gray-700 dark:divide-gray-600" ref="popoverDropdownItemRef">
+            <div class="py-1">
+            <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Separated link</a>
+            </div>
+            <div class="py-1">
+            <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Separated link</a>
+            </div>
         </div>
     </div>
 </template>
+<style>
+    #percent {
+        font-size: 0.5rem;
+        top: 9px;
+        right: 6px;
+    }
+</style>
 <script>
+import { createPopper } from "@popperjs/core"
+import ProgressRing from "./ProgressRing.vue"
     export default {
+        components: {
+            ProgressRing
+        },
+        data(){
+            return {
+                progress: 0,
+                dropdownPopoverShow: false
+            }
+        },
         props: {
             listMoviePopular: Object
         },
+        mounted() {
+            // emulating progress
+            const interval = setInterval(() => {
+            this.progress += 1;
+            if (this.progress === Math.round(this.$props.listMoviePopular.vote_average * 100)/10)
+                clearInterval(interval);
+            }, 1000);
+            console.log(Math.round(this.$props.listMoviePopular.vote_average * 100)/10)
+        },
         methods: {
-            valeurProgress () {
-                return {
-                    init () {
-                        this.percent = Math.round(this.$props.listMoviePopular.vote_average * 100)
-                    },
-                    circumference: 20 * 2 * Math.PI,
+            toggleDropdownBtn: function () {
+                if (this.dropdownPopoverShow) {
+                    this.dropdownPopoverShow = false;
+                } else {
+                    this.dropdownPopoverShow = true;
+                    createPopper(this.$refs.btnDropdownBtnRef, this.$refs.popoverDropdownItemRef, {
+                        placement: "bottom-start"
+                    });
                 }
-                console.log(this.$props.listMoviePopular.vote_average)
             }
         }
     }
---
+++ tmdb/components/MoviesPopular/ProgressRing.vue
@@ -0,0 +1,40 @@
+<template>
+    <svg
+    :height="radius * 2"
+    :width="radius * 2"
+  >
+    <circle
+      id="progress-ring"
+      stroke="yellow"
+      fill="transparent"
+      :stroke-dasharray="circumference + ' ' + circumference"
+      :style="{ strokeDashoffset }"
+      :stroke-width="stroke"
+      :r="normalizedRadius"
+      :cx="radius"
+      :cy="radius"
+    />
+  </svg>
+</template>
+<script>
+    export default {
+        props: {
+            radius: Number,
+            progress: Number,
+            stroke: Number,
+        },
+        data() {
+            const normalizedRadius = this.radius - this.stroke * 2
+            const circumference = normalizedRadius * 2 * Math.PI
+            return {
+                normalizedRadius,
+                circumference
+            }
+        },
+        computed: {
+            strokeDashoffset() {
+                return this.circumference - this.progress / 100 * this.circumference;
+            }
+        }
+    }
+</script>
